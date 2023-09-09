9175f25e2b4f77324f88897f3683d1aaee08d5da
Jared Hughes <jahughes241@gmail.com>
2023-09-09 15:14:41 -0700

    Add proper language setup

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,37 @@ import { parser } from "./syntax.grammar";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
 import { styleTags, tags as t } from "@lezer/highlight";
 
+const kwsString =
+  "pub test comptime export extern inline noinline threadlocal " +
+  "usingnamespace fn const var nosuspend suspend defer errdefer else or and " +
+  "break continue resume return async error anyframe unreachable packed " +
+  "switch asm volatile linksection addrspace callconv noalias anytype if " +
+  "while for orelse catch try await allowzero align struct opaque enum union word";
+
+const kws = kwsString.split(" ");
+
 export const zigLanguage = LRLanguage.define({
   parser: parser.configure({
     // TODO: correct config here
     props: [
       styleTags({
-        "Identifier IdentifierSuffix Constant ConstantSuffix": t.variableName,
-        "InstanceVariable ClassVariable GlobalVariable": t.variableName,
-        "Integer Float Copmlex Rational UnaryLiteral": t.number,
+        Identifier: t.variableName,
+        kwsString: t.keyword,
+        BuiltinIdentifier: t.standard(t.variableName),
+        StringLiteral: t.string,
+        Float: t.float,
+        Integer: t.integer,
+        CharLiteral: t.character,
         "( )": t.paren,
         "[ ]": t.squareBracket,
         "{ }": t.brace,
-        Comment: t.comment,
+        "DocComment ContainerDocComment LineComment": t.comment,
       }),
     ],
   }),
   languageData: {
-    commentTokens: { line: "#" },
+    commentTokens: { line: "//" },
+    autocomplete: kws.map((kw) => ({ label: kw, type: "keyword" })),
   },
 });
 
